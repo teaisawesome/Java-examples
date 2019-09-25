bce366de0d84d27fc032f88b04799173d78701c6
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2019-09-25 23:23:06 +0200

    Java Example - Type converting

---
+++ examples/typeconv.java
@@ -0,0 +1,11 @@
+public class MyClass
+{
+    public static void main(String[] args) {
+
+        int x = 5, y = 2, z = x/y;
+
+        double d = x/y, d2 = x/(double)y;
+
+        System.out.println("int z="+z+", double d="+d+", double d2="+d2);
+    }
+}
