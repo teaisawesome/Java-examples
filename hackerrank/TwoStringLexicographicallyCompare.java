4afcaebdd705c92359dee634d0cd32fd30335a67
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2021-07-02 19:13:55 +0200

    Create TwoStringLexicographicallyCompare.java

---
+++ hackerrank/TwoStringLexicographicallyCompare.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        
+        int aLength = A.length();
+        int bLength = B.length();
+        
+        System.out.println(aLength + bLength);
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
+        
+    }
+}
